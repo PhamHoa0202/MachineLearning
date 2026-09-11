66f1ee1ddbd1e2175a08c770199c13564bdaee33
PhamHoa0202 <phamhoa02022005@gmail.com>
2026-09-11 19:57:38 +0700

    Thuc hien phep tinh

---
+++ btap1.py
@@ -0,0 +1,9 @@
+a = int(input("Nhập số a: "))
+b = int(input("Nhập số b: "))
+print("Cộng 2 số = ", a+b)
+print("Trừ 2 số = ", a-b)
+print("Nhân 2 số = ", a*b)
+if b != 0:
+    print("Chia 2 số = ",a/b)
+else:
+    print("Không thực hiện được!")
